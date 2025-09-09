ddbe57be860d49dd04b4f01141c8560edf880b31
Amir-h89 <amir.h.mohammadaz@gmail.com>
2025-09-09 20:44:44 +0330

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a;
+    float b;
+
+    cout << "ax+b" << endl;
+    cout<<"a: ";
+    cin>>a;
+
+    cout<<"b: ";
+    cin >>b;
+
+    cout << "x = "<< b / a;
+
+    return 0;
+}
