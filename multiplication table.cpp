e89f5ffce54361bca2bc6a5a915e437dd506ac81
Amir-h89 <amir.h.mohammadaz@gmail.com>
2025-09-10 16:28:08 +0330

    Add files via upload

---
+++ multiplication table.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+
+
+for(int x = 1; x<=10;x++){
+cout<<"********************"<<endl;
+for(int y = 0; y<=10;y++){
+cout<< x <<" * "<< y <<" = "<<x * y<<endl;
+}
+}
+
+    return 0;
+}
